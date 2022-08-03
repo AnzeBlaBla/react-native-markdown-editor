ff76c41b1e1326f3df6e9118ebe5ed8289a3e322
AnzeBlaBla <anzemajblabla@gmail.com>
2022-08-03 12:08:48 +0200

    Fixed list format

--- src/applyListFormat.js
+++ src/applyListFormat.js
@@ -12,16 +12,16 @@ export default ({ getState, item, setText, setSelection }) => {
       selection,
       `${item.prefix} ${text.substring(selection.start, selection.end)}\n`,
     );
-    newSelection = { start: selection.end + 3, end: selection.end + 3 };
+    newSelection = { start: selection.end + item.prefix.length + 2, end: selection.end + item.prefix.length + 2 };
   } else if (
     selection.start === selection.end &&
     text.substring(selection.end - 1, selection.end) === '\n'
   ) {
     newText = replaceBetween(text, selection, `${item.prefix} `);
-    newSelection = { start: selection.start + 2, end: selection.start + 2 };
+    newSelection = { start: selection.start + item.prefix.length + 1, end: selection.start + item.prefix.length + 1 };
   } else {
     newText = replaceBetween(text, selection, `\n${item.prefix} `);
-    newSelection = { start: selection.start + 3, end: selection.start + 3 };
+    newSelection = { start: selection.start + item.prefix.length + 2, end: selection.start + item.prefix.length + 2 };
   }
 
   setText(newText);
